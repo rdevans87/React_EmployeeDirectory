d7db0392c89bd3bdb6ce608a8d738005d7cfe9d7
Ryan Evans <rdevans87@gmail.com>
2021-06-22 17:34:08 -0400

    updates axios error

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default {
 
 getRandomUser: function(){
-
-    return.axios.get("https://randomuser.me/api/?results=200&nat=us");
+    return axios.get("https://randomuser.me/api/?results=200&nat=us");
 }
+
+};

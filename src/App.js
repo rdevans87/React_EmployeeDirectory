02d380c633dc7f64a97804f0cee9ead24d4e6146
Ryan Evans <rdevans87@gmail.com>
2021-06-23 04:23:28 -0400

    wrong syntax in app js completed search component

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import "./App.css"
 function App() {
   return (
     <div className = "App">
-      <Wrapper/>
+      <Wrapper>
       <Header/>
       <Main/>
-      <Wrapper/>
+      </Wrapper>
     </div>
 
 
---
+++ src/components/Search/Search.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/Searchbox.css";
+
+export default class Search extends React.Component {
+render() {
+    return (
+        <div>    
+            <form onSubmit={this.props.handleSubmit}>
+            <input>
+                name="search"
+                value={this.props.search}
+                onChange={this.props.handleInputChange}
+            </input>
+
+        <button>
+
+            Search
+        
+            </button>
+        </form>
+    </div>
+
+    )
+
+    };
+}
+
+export default Search;

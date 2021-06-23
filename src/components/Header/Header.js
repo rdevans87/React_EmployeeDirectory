87c1175c7a4f7901c5448b4dd152e8c5848b9e74
Ryan Evans <rdevans87@gmail.com>
2021-06-23 04:09:50 -0400

    header component complete with bootstrap framework

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,21 @@ import "../styles/Header.css";
 
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
-export default class Header extends Component.
-function Header() {
-  return (
-    <header className="header">
-      <h1>Employee Directory</h1>
-    </header>
-  );
-}
+export default class Header extends React.Component {
+render() {
+    return (
+      <div>
+        <div className="jumbo jumbotron-fluid">
+          <div className="container">
+            <h1 className="text-center text-white">Employee Directory</h1>
+            <p className="lead text-center text-white">Search for Employees. Sort by category or filter by property</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+};
 
-export default Header;
+
+
+          export default Header;

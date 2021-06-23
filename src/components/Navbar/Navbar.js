a1b422bb88d5b30df27bde09500f6a9bab6f6ce9
Ryan Evans <rdevans87@gmail.com>
2021-06-23 06:10:43 -0400

    navbar setup

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Search  from "../Search/Search"
 import "../Navbar/Navbar.css";
 
 // By importing the Navbar.css file, it is added to the DOM whenever this component loads
 
 function Navbar({ handleSearchChange }) {
   return (
-    <nav className="navbar navbar-expand navbar bg-dark">
-      <div
-      <a href="/">Welcome</a>
+    <nav className="navbar navbar-fixed-top navbar bg-dark">
+      <div className="navbar navbar-row">
+      <Search handleSearchChange={handleSearchChange} />
+    </div>
     </nav>
   );
 }

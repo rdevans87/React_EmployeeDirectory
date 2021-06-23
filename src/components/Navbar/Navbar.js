85627145a990bfbf58b40957a85abc0dd67b9ed6
Ryan Evans <rdevans87@gmail.com>
2021-06-23 06:05:02 -0400

    navbar update to styling components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,9 +3,10 @@ import "../Navbar/Navbar.css";
 
 // By importing the Navbar.css file, it is added to the DOM whenever this component loads
 
-function Navbar() {
+function Navbar({ handleSearchChange }) {
   return (
-    <nav className="navbar">
+    <nav className="navbar navbar-expand navbar bg-dark">
+      <div
       <a href="/">Welcome</a>
     </nav>
   );

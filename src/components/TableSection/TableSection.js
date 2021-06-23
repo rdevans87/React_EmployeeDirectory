30d8c6c4269e10350b702c4ce3f81b702186d06b
Ryan Evans <rdevans87@gmail.com>
2021-06-23 06:35:40 -0400

    working through TableSection for search results

--- src/components/TableSection/TableSection.js
+++ src/components/TableSection/TableSection.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "../Header/Header"
+import 
 import ResultsTable from "../ResultsTable/ResultsTable";
 import TableHeader from "../TableHeader/TableHeader";
 import TableData from "../TableData/TableData";
-import getEmployeeResults from "../../utils/API";  
+import API from "../../utils/API";  
 import "./TableSection.css";
 
 export default class TableSection extends React.Component {
@@ -13,12 +14,26 @@ export default class TableSection extends React.Component {
     employee: []
   };
 
-}
+  componentDidMount() {
+    API.getEmployees().then(results => {
+      this.setState({
+      employees: results.data.results,
+      filteredEmployees: results.data.results
+      });
+    
+    });
+  }
 
 
 
 
-)
+
+
+
+
+
+
+
 
 };
 

03d56658a8913ee70e050e852e4db19e8d1fe417
Ryan Evans <rdevans87@gmail.com>
2021-06-23 11:47:47 -0400

    deleting search results tab in favor of results datat

--- src/components/DataSection/DataSection.js
+++ src/components/DataSection/DataSection.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../Header/Header"
-import 
 import ResultsTable from "../ResultsTable/ResultsTable";
 import TableHeader from "../TableHeader/TableHeader";
 import ResultsData from "../ResultsData/ResultsData";

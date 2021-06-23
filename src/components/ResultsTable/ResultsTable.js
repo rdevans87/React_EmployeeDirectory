c5a675ae70a729600d02155fec34c1a331fd1f8f
Ryan Evans <rdevans87@gmail.com>
2021-06-23 06:57:32 -0400

    may need to refactor components

--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,2 +1,17 @@
 import React from "react";
 import TableHeader from "..TableHeader/TableHeader";
+import TableSection from "..TableSection/TableSection";
+import "./ResultsTable.css";
+
+function ResultsTable({ tableHeader, employees, handleSort}) {
+return ( className="table table-info mt-5"
+
+
+
+
+
+)
+
+
+
+

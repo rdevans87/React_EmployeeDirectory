82cffcf1519caa54cbd51f208a0a17d414ef2271
Ryan Evans <rdevans87@gmail.com>
2021-06-23 12:07:06 -0400

    switch code snippets to different component

--- src/components/DataSection/DataSection.js
+++ src/components/DataSection/DataSection.js
@@ -30,6 +30,52 @@ export default class DataSection extends React.Component {
 
 
 
+  // When the component mounts, update the title to be Wikipedia Searcher
+// useEffect(() => {
+//     document.title = "Employee Search";
+
+
+// if (!search) {
+//     return;
+//   }
+
+//   API.searchTerms(search)
+//     .then(res => {
+//       if (res.data.length === 0) {
+//         throw new Error("No results found.");
+//       }
+//       if (res.data.status === "error") {
+//         throw new Error(res.data.message);
+
+
+
+
+// const handleInputChange = event => {
+//     setSearch(event.target.value);
+//   };
+
+//   const handleFormSubmit = event => {
+//     event.preventDefault();
+//   };
+//   return (
+//     <div>
+//       <Container style={{ minHeight: "100vh" }}>
+//         <h1 className="text-center">Search For Anything on Wikipedia</h1>
+//         <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 10 }}>
+//           {error}
+//         </Alert>
+//         <SearchForm
+//           handleFormSubmit={handleFormSubmit}
+//           handleInputChange={handleInputChange}
+//           results={search}
+//         />
+//         <SearchResults title={title} url={url} />
+//       </Container>
+//     </div>
+
+
+
+
 
 
 

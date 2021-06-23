b6db7bd899f6b479515ed9e7e10b4da1ae83a2bf
Ryan Evans <rdevans87@gmail.com>
2021-06-23 08:53:01 -0400

    deleting unused components for refactor

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import React from "./components/Main";
 import Header from "./components/Wrapper";
 import Wrapper from "./components/Header";
+import Footer from "./components/Footer";
+import Search from "./components/Search";
+import Navbar from "./components/Navbar";
 
 import "./App.css" 
 
 function App() {
+  document.title = "Employee Directory"
   return (
     <div className = "App">
+      <
       <Wrapper>
       <Header/>
       <Main/>

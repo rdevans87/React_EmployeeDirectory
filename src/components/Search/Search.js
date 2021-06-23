ed29176848a53212e627bf7b893c9e01c0c21cda
Ryan Evans <rdevans87@gmail.com>
2021-06-23 04:25:41 -0400

    fixed error in search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,25 +2,22 @@ import React from "react";
 import "../styles/Searchbox.css";
 
 export default class Search extends React.Component {
-render() {
-    return (
-        <div>    
-            <form onSubmit={this.props.handleSubmit}>
-            <input>
-                name="search"
-                value={this.props.search}
-                onChange={this.props.handleInputChange}
-            </input>
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.props.handleSubmit}>
+                    <input
+                        name="search"
+                        value={this.props.search}
+                        onChange={this.props.handleInputChange}>
+                    </input>
+                    <button>
+                        Search
+                    </button>
+                </form>
+            </div>
 
-        <button>
-
-            Search
-        
-            </button>
-        </form>
-    </div>
-
-    )
+        )
 
     };
 }

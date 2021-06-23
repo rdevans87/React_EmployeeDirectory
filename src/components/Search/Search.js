f04402164fb97048c03d734d81efb5dd6ef63bc8
Ryan Evans <rdevans87@gmail.com>
2021-06-23 08:25:45 -0400

    carried over code from wikipedia search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -22,4 +22,31 @@ export default class Search extends React.Component {
     };
 }
 
+
+
+
+
+
+// const handleInputChange = event => {
+//     setSearch(event.target.value);
+//   };
+
+//   const handleFormSubmit = event => {
+//     event.preventDefault();
+//   };
+//   return (
+//     <div>
+//       <Container style={{ minHeight: "100vh" }}>
+//         <h1 className="text-center">Search For Anything on Wikipedia</h1>
+//         <Alert type="danger" style={{ opacity: error ? 1 : 0, marginBottom: 10 }}>
+//           {error}
+//         </Alert>
+//         <SearchForm
+//           handleFormSubmit={handleFormSubmit}
+//           handleInputChange={handleInputChange}
+//           results={search}
+//         />
+//         <SearchResults title={title} url={url} />
+//       </Container>
+//     </div>
 export default Search;

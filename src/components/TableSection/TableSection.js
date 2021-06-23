6f75ec574eb2a0be6a14359c7ebf9ed42484ec40
Ryan Evans <rdevans87@gmail.com>
2021-06-23 05:07:30 -0400

    created components for search results, table section, table data

---
+++ src/components/TableSection/TableSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/TableSection.css";
+
+// By importing the Section.css file, it is added to the DOM whenever this component loads
+function Section() {
+  return (
+    <section className="tableSection">
+      <h2>Lorem Ipsum Dolor Sit Amet</h2>
+
+
+export default Section;

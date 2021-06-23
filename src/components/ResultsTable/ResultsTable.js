5e382ca36647d0bfd0407ce107a0c29f94e6736f
Ryan Evans <rdevans87@gmail.com>
2021-06-23 07:20:04 -0400

    working through table creation with bootsrap

--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,10 +1,37 @@
 import React from "react";
 import TableHeader from "..TableHeader/TableHeader";
 import TableSection from "..TableSection/TableSection";
+import TableData from "../TableData/TableData";
 import "./ResultsTable.css";
 
-function ResultsTable({ tableHeader, employees, handleSort}) {
-return ( className="table table-info mt-5"
+exports default class TableSection extends React.Component {
+
+
+ render() {
+    return (
+        <div>
+           <table className="table table-info mt-5">
+           <thead classname="table-header thead-light">
+           
+           
+           
+           
+           
+           </thead>
+           
+           
+           
+           
+           </table>
+ }
+
+
+
+
+
+}
+
+
 
 
 

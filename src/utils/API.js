06bcecdc9155b4254a8a662b279a973d364fa3d9
Ryan Evans <rdevans87@gmail.com>
2021-06-23 08:38:14 -0400

    cleaning repo deleting duplicate files

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,8 +2,12 @@ import axios from "axios";
 
 export default {
 
-getRandomUser: function(){
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+getRandomUser: function(query){
+    // return axios.get("https://randomuser.me/api/?results=200&nat=us"
+    // query +
+    // "&limit=1&format=json&origin=*"
+    
+    );
 }
 
 };

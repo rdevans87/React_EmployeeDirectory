a5ba39a4e755d3c1699b7a6aa526a6982498f617
Ryan Evans <rdevans87@gmail.com>
2021-06-24 12:41:44 -0400

    changing table data

--- src/components/TableData/TableData.js
+++ src/components/TableData/TableData.js
@@ -4,38 +4,33 @@ export default class TableData extends React.Component {
 
   render() {
     return (
-      tbody>
+      <tbody>
       {users[0] !== undefined && users[0].name !== undefined ? (
         users.map(({ login, name, picture, phone, email, dob }) => {
           return (
             <tr key={login.uuid}>
               <td data-th="Image" className="align-middle">
-                <img
-         
-              < tr >
-                          <th scope="row"></th>
-
-                                <td>
-                                    <img
-                                        src={employee.picture.medium}
-                                        alt='employee'
-                                    />
-                                </td>
+                <img      
+                src={picture.medium}
+                alt={"profile image for " + name.first + " " + name.last}
+                className="img-responsive"
+              />
+                </td>    
 
                                 <td>{employee.name.first} {employee.name.last}</td>
-                                <td>{employee.phone}</td>
+                                 <td>{employee.phone}</td>
                                 <td>{employee.email}</td>
                                 <Moment format="MM/DD/YYYY">
                                     <td>{employee.dob.date}</td>
                               </Moment>
                             </tr>
 
-                        ))}
+                        )}
 
                     </tbody>
                 </table>
             </div >
-        )
+        ) )
     }
 }
         

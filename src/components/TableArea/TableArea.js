6841a7cab75051f23694e26782494fad30be0021
Ryan Evans <rdevans87@gmail.com>
2021-06-24 02:18:43 -0400

    TableSection changed to table for better file conventions

--- src/components/TableArea/TableArea.js
+++ src/components/TableArea/TableArea.js
@@ -10,11 +10,11 @@ import "./TableSection.css";
 
 
 export default class TableArea extends React.Component {
-  
+
   state = {
-    search:"names",
+    search: "names",
     employees: []
-  
+
   }
 
 
@@ -22,109 +22,111 @@ export default class TableArea extends React.Component {
     this.loadEmployees();
   }
 
-    handleInputChange = (event) => {
-      const { name, value } = event.target;
-      this.setState(
-        {
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState(
+      {
 
-          [name]: value
+        [name]: value
 
-        }
+      }
     )
-}     
+  }
 
-loadEmployees = () => [
-  getEmployeeName()
-    .then((response) => {
-      console.log(response);
-      this.setState(
-        {
-          names: response.data.results
-        }
-      )
-   
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+  loadEmployees = () => [
+    getEmployeeName()
+      .then((response) => {
+        console.log(response);
+        this.setState(
+          {
+            names: response.data.results
+          }
+        )
 
-]
+      })
+      .catch((err) => {
+        console.log(err)
+      })
 
-
-searchEmployee = () => [
-  getEmployeeName()
-    .then((response) => {
-
-      console.log(response);
-
-      let filter = this.state.search;
-
-      let filteredList = response.data.results.filter(item => {
-        let values = Object.values(item.name.first)
-          .join("")
-          .toLowerCase();
-        return values.indexOf(filter.toLowerCase()) !== -1;
-      });
-
-      this.setState(
-        {
-          names: filteredList
-        }
-
-      )
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  
-]
+  ]
 
 
-handleInputSubmit = (event) => {
-  event.preventDefault();
-  console.log("Name")
-// const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
+  searchEmployee = () => [
+    getEmployeeName()
+      .then((response) => {
 
-// const BASEURL = "https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole";
-  this.searchName();
+        console.log(response);
 
-};
+        let filter = this.state.search;
+
+        let filteredList = response.data.results.filter(item => {
+          let values = Object.values(item.name.first)
+            .join("")
+            .toLowerCase();
+          return values.indexOf(filter.toLowerCase()) !== -1;
+        });
+
+        this.setState(
+          {
+            names: filteredList
+          }
+
+        )
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+  ]
+
+
+  handleInputSubmit = (event) => {
+    event.preventDefault();
+    console.log("Name")
+    // const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
+
+    // const BASEURL = "https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole";
+    this.searchName();
+
+  };
 
 
 
   SortByName = (event) => {
-  event.preventDefault();
-  console.log("Listening!")
+    event.preventDefault();
+    console.log("Listening!")
 
 
   }
 
-//    const handleSort = (parameter) => {
-//       (order === 'asc') ? setOrder('desc') : setOrder('asc')
-//        const newSortedList = listUtils.sortList(employees, parameter, order)
-//         setEmployees(newSortedList)  
-// }
-  
+  //    const handleSort = (parameter) => {
+  //       (order === 'asc') ? setOrder('desc') : setOrder('asc')
+  //        const newSortedList = listUtils.sortList(employees, parameter, order)
+  //         setEmployees(newSortedList)  
+  // }
 
 
-render() {
-return(
+
+  render() {
+    return (
       <div className="wrapper" >
-        
-        <Header/>
-     <SearchBar
-     search={this.state.search}
-      handleInputChange={this.handleInputChange}
-      handleSubmit={this.handleInputSubmit}
-      />
 
-    <TableHeader SortByName={this.SortByName} />
-      <TableData
-       names={this.state.employees}
-      />
-  
-   </div>
+        <Header />
+        <SearchBar
+          search={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleSubmit={this.handleInputSubmit}
+        />
 
-)
+        <TableHeader SortByName={this.SortByName} />
+        <TableData
+          names={this.state.employees}
+        />
+
+      </div>
+
+    )
+
+  }
 
 }

283061058462b209614e27eac754315f79ad10fc
Ryan Evans <rdevans87@gmail.com>
2021-06-24 00:10:59 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "../Header/Header.css";
+import  "./Header.css";
 
 // By importing the Header.css file, it is added to the DOM whenever this component loads
 
@@ -14,8 +14,12 @@ render() {
           </div>
         </div>
       </div>
+    
     );
+
+
   };
 };
+
 
 
